0014568eae9836e4c5feba1e12e16cb236f9ec79
binbinyouli <binbinyouli27@gmail.com>
2016-12-05 19:11:14 +0800

    update sql

--- employee.sql
+++ employee.sql
@@ -26,22 +26,15 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `birthday` date DEFAULT NULL,
   `nationality` varchar(255) DEFAULT NULL,
   `interest` varchar(255) DEFAULT NULL,
+  `salary` DOUBLE NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- 正在导出表  demo.employee 的数据：~8 rows (大约)
 DELETE FROM `employee`;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` (`id`, `name`, `code`, `sex`, `age`, `birthday`, `nationality`, `interest`) VALUES
-	(4, 'kong', 'sda', 'female', 12, '2016-03-12', 'dada', 'dada'),
-	(14, 'one1', 'dada', 'male', 12, '2016-03-14', 'dada', 'basketball;football;running;'),
-	(15, 'dafda', 'fdasfdsa', 'male', 12, '2016-03-26', NULL, 'football;running;other;'),
-	(16, 'two', 'twos', 'male', 11, '2016-03-12', NULL, 'basketball;football;running;'),
-	(17, 'one2', 'dada', 'male', 12, '2016-03-14', 'dada', 'basketball;football;running;other;'),
-	(18, 'one3', 'dada', 'male', 12, '2016-03-14', 'dada', 'basketball;football;'),
-	(19, 'one3', 'dada', 'male', 12, '2016-03-14', 'dada', 'football;running;'),
-	(20, 'one', 'dada', 'female', 12, '2016-03-14', 'dada', 'frfd');
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+INSERT INTO `employee` (`id`, `name`, `code`, `sex`, `age`, `birthday`, `nationality`, `interest`, `salary`) VALUES
+	(1, 'kong', 'sda', 'female', 12, '2016-03-12', 'dada', 'dada', 12323.23);
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
